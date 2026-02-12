bab43273af962df6f27c2cf57ac22bae677e3c20
erob05 <ericbenroberts@gmail.com>
2026-02-12 17:30:59 -0600

    Finished index.js for M03

--- projects/eroberts_M03project/index.js
+++ projects/eroberts_M03project/index.js
@@ -18,8 +18,18 @@ if (args.length < 2 ) {
     process.exit(1);
 }
 
-const world = args.slice(0, args.length - 1).join(" ");
-const weapon = args[args.length - 1];
+let weapon = "";
+let world = "";
+
+const lastTwo = args.slice(-2).join(" ").toLowerCase();
+if (lastTwo === "death note") {
+    weapon = "Death Note";
+    world = args.slice(0, -2).join(" ");
+} else {
+    weapon = args[args.length - 1];
+    world = args.slice(0, -1).join(" ");
+}
+
 
 // Input check
 const worldLower = world.toLowerCase();
@@ -44,7 +54,23 @@ if (!validWeapons.includes(weaponLower)) {
 let story = `You blast through a portal sending you to ${world} wielding your ${weapon}. \n`;
 
 if (worldLower === "the shire" && weaponLower === "lightsaber") {
-    story += "Gandalf joins you as you battle an army of the Sith!"
+    story += "Gandalf joins you as you battle an army of the Sith! You saved the Shire!";
 } else if (worldLower === "the shire" && weaponLower === "death note") {
-    story+= "Frodo looks nervous as you write names in the Death Note!"
+    story+= "Frodo looks nervous as you write names in the Death Note! You realize you don't know Sauron's last name. The Shire is lost to the dark forces...";
+} else if (worldLower === "hogwarts" && weaponLower === "lightsaber") {
+    story += "Dumbledore trains you in magical wizard lightsaber combat! It's not enough as Voldemort just Avada Kedavra's you. Hogwarts is lost to the dark forces...";
+} else if (worldLower === "hogwarts" && weaponLower === "death note") {
+    story += "Everyone is terrified by your notebook of doom. You write Voldemort's name swiftly putting an end to the war. You saved Hogwarts!"
 }
+
+const outputFile = "adventure.result.txt";
+fs.writeFile(outputFile, story, err => {
+    if (err) {
+        console.error("Error writing file:", err);
+        return;
+    }
+    console.log(`\nYour adventure has been saved to '${outputFile}'!`);
+
+    const message = fs.readFileSync(outputFile, 'utf8');
+    console.log(message);
+});
